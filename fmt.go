626f0aa91658b0df67d60b71c4a398c84ebeeeac
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-02-24 11:22:04 +0700

    feat: fmt.go

---
+++ fmt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	firstName := "Yusuf"
+	lastName := "Supriadi"
+
+	fmt.Println("Hallo %s %s! \n", firstName, lastName)
+}
